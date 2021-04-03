a2cc220628b5b5b77962cb80fefc35e04564f8c5
florian schweizer <florian.schweizer@tum.de>
2021-04-03 17:28:27 +0200

    Initial Commit

---
+++ UsingAPIs/UsingAPIsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingAPIsApp.swift
+//  UsingAPIs
+//
+//  Created by florian schweizer on 03.04.21.
+//
+
+import SwiftUI
+
+@main
+struct UsingAPIsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
